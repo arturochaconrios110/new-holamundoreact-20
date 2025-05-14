2850ff6eb11f84e3beebb93de0e63e5905bc3bb5
arturochaconrios110 <arturo.chacon.rios@estudiante.ipss.cl>
2025-05-14 17:26:11 -0400

    revision

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {TextField} from '@mui/material/TextField';
 
 function App() {
   return (
@@ -30,8 +31,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+
     </div>
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+  
   );
 }
 
